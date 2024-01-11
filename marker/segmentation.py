bdb86dcaf4ff746f0abc3e44f1ab36d87c55c731
遥奕 <jibing.yjb@alibaba-inc.com>
2024-01-11 17:07:53 +0800

    optimize code

--- marker/segmentation.py
+++ marker/segmentation.py
@@ -17,7 +17,9 @@ from math import isclose
 # Otherwise some images can be truncated
 Image.MAX_IMAGE_PIXELS = None
 
-processor = LayoutLMv3Processor.from_pretrained(settings.LAYOUT_MODEL_NAME, apply_ocr=False)
+processor = LayoutLMv3Processor.from_pretrained(
+    settings.LAYOUT_MODEL_NAME, apply_ocr=False
+)
 
 CHUNK_KEYS = ["input_ids", "attention_mask", "bbox", "offset_mapping"]
 NO_CHUNK_KEYS = ["pixel_values"]
@@ -40,24 +42,32 @@ def load_segment_model():
         7: "Section-header",
         8: "Table",
         9: "Text",
-        10: "Title"
+        10: "Title",
     }
 
     model.config.label2id = {v: k for k, v in model.config.id2label.items()}
     return model
 
 
-def get_pages_types(doc, blocks: List[Page], segment_model, batch_size=settings.LAYOUT_BATCH_SIZE):
-    encodings, metadata, sample_lengths = get_features(doc, blocks)
+def get_pages_types(
+    doc, pages: List[Page], segment_model, batch_size=settings.LAYOUT_BATCH_SIZE
+) -> List[List[BlockType]]:
+    encodings, metadata, sample_lengths = get_features(doc, pages)
     predictions = predict_block_types(encodings, segment_model, batch_size)
-    block_types = match_predictions_to_boxes(encodings, predictions, metadata, sample_lengths, segment_model)
-    assert len(block_types) == len(blocks)
-    return block_types
+    pages_types = match_predictions_to_boxes(
+        encodings, predictions, metadata, sample_lengths, segment_model
+    )
+    assert len(pages_types) == len(pages)
+    return pages_types
 
 
 def get_provisional_boxes(pred, box, is_subword, start_idx=0):
-    prov_predictions = [pred_ for idx, pred_ in enumerate(pred) if not is_subword[idx]][start_idx:]
-    prov_boxes = [box_ for idx, box_ in enumerate(box) if not is_subword[idx]][start_idx:]
+    prov_predictions = [pred_ for idx, pred_ in enumerate(pred) if not is_subword[idx]][
+        start_idx:
+    ]
+    prov_boxes = [box_ for idx, box_ in enumerate(box) if not is_subword[idx]][
+        start_idx:
+    ]
     return prov_predictions, prov_boxes
 
 
@@ -73,7 +83,7 @@ def get_page_encoding(page, page_blocks: Page):
     png = pix.pil_tobytes(format="PNG")
     png_image = Image.open(io.BytesIO(png))
     # If it is too large, make it smaller for the model
-    rgb_image = png_image.convert('RGB')
+    rgb_image = png_image.convert("RGB")
     rgb_width, rgb_height = rgb_image.size
 
     # Image is correct size wrt the pdf page
@@ -109,9 +119,9 @@ def get_page_encoding(page, page_blocks: Page):
     boxes = [normalize_box(box, pwidth, pheight) for box in boxes]
     for box in boxes:
         # Verify that boxes are all valid
-        assert(len(box) == 4)
-        assert(max(box)) <= 1000
-        assert(min(box)) >= 0
+        assert len(box) == 4
+        assert (max(box)) <= 1000
+        assert (min(box)) >= 0
 
     encoding = processor(
         rgb_image,
@@ -123,26 +133,34 @@ def get_page_encoding(page, page_blocks: Page):
         stride=settings.LAYOUT_CHUNK_OVERLAP,
         padding="max_length",
         max_length=settings.LAYOUT_MODEL_MAX,
-        return_overflowing_tokens=True
+        return_overflowing_tokens=True,
     )
-    offset_mapping = encoding.pop('offset_mapping')
-    overflow_to_sample_mapping = encoding.pop('overflow_to_sample_mapping')
+    offset_mapping = encoding.pop("offset_mapping")
+    overflow_to_sample_mapping = encoding.pop("overflow_to_sample_mapping")
     bbox = list(encoding["bbox"])
     input_ids = list(encoding["input_ids"])
     attention_mask = list(encoding["attention_mask"])
     pixel_values = list(encoding["pixel_values"])
 
-    assert len(bbox) == len(input_ids) == len(attention_mask) == len(pixel_values) == len(offset_mapping)
+    assert (
+        len(bbox)
+        == len(input_ids)
+        == len(attention_mask)
+        == len(pixel_values)
+        == len(offset_mapping)
+    )
 
     list_encoding = []
     for i in range(len(bbox)):
-        list_encoding.append({
-            "bbox": bbox[i],
-            "input_ids": input_ids[i],
-            "attention_mask": attention_mask[i],
-            "pixel_values": pixel_values[i],
-            "offset_mapping": offset_mapping[i]
-        })
+        list_encoding.append(
+            {
+                "bbox": bbox[i],
+                "input_ids": input_ids[i],
+                "attention_mask": attention_mask[i],
+                "pixel_values": pixel_values[i],
+                "offset_mapping": offset_mapping[i],
+            }
+        )
 
     other_data = {
         "original_bbox": boxes,
@@ -152,12 +170,12 @@ def get_page_encoding(page, page_blocks: Page):
     return list_encoding, other_data
 
 
-def get_features(doc, blocks):
+def get_features(doc, pages: List[Page]):
     encodings = []
     metadata = []
     sample_lengths = []
-    for i in range(len(blocks)):
-        encoding, other_data = get_page_encoding(doc[i], blocks[i])
+    for i in range(len(pages)):
+        encoding, other_data = get_page_encoding(doc[i], pages[i])
         encodings.extend(encoding)
         metadata.append(other_data)
         sample_lengths.append(len(encoding))
@@ -189,16 +207,18 @@ def predict_block_types(encodings, segment_model, batch_size):
     return all_predictions
 
 
-def match_predictions_to_boxes(encodings, predictions, metadata, sample_lengths, segment_model) -> List[List[BlockType]]:
+def match_predictions_to_boxes(
+    encodings, predictions, metadata, sample_lengths, segment_model
+) -> List[List[BlockType]]:
     assert len(encodings) == len(predictions) == sum(sample_lengths)
     assert len(metadata) == len(sample_lengths)
 
     page_start = 0
-    page_block_types = []
+    pages_types = []
     for pnum, sample_length in enumerate(sample_lengths):
         # Page has no blocks
         if sample_length == 0:
-            page_block_types.append([])
+            pages_types.append([])
             continue
 
         page_data = metadata[pnum]
@@ -216,47 +236,54 @@ def match_predictions_to_boxes(encodings, predictions, metadata, sample_lengths,
         for i in range(len(token_boxes)):
             assert len(token_boxes[i]) == len(page_predictions[i])
 
-        for i, (pred, box, mapped) in enumerate(zip(page_predictions, token_boxes, offset_mapping)):
+        for i, (pred, box, mapped) in enumerate(
+            zip(page_predictions, token_boxes, offset_mapping)
+        ):
             box = box.tolist()
             is_subword = np.array(mapped)[:, 0] != 0
             overlap_adjust = 0
             if i > 0:
-                overlap_adjust = 1 + settings.LAYOUT_CHUNK_OVERLAP - sum(is_subword[:1 + settings.LAYOUT_CHUNK_OVERLAP])
+                overlap_adjust = (
+                    1
+                    + settings.LAYOUT_CHUNK_OVERLAP
+                    - sum(is_subword[: 1 + settings.LAYOUT_CHUNK_OVERLAP])
+                )
 
-            prov_predictions, prov_boxes = get_provisional_boxes(pred, box, is_subword, overlap_adjust)
+            prov_predictions, prov_boxes = get_provisional_boxes(
+                pred, box, is_subword, overlap_adjust
+            )
 
             for prov_box, prov_prediction in zip(prov_boxes, prov_predictions):
                 if prov_box == [0, 0, 0, 0]:
                     continue
                 block_type = BlockType(
                     block_type=segment_model.config.id2label[prov_prediction],
-                    bbox=prov_box
+                    bbox=prov_box,
                 )
 
                 # Sometimes blocks will cross chunks, unclear why
-                if len(predicted_block_types) == 0 or prov_box != predicted_block_types[-1].bbox:
+                if (
+                    len(predicted_block_types) == 0
+                    or prov_box != predicted_block_types[-1].bbox
+                ):
                     predicted_block_types.append(block_type)
 
         # Align bboxes
         # This will search both lists to find matching bboxes
         # This will align both sets of bboxes by index
         # If there are duplicate bboxes, it may result in issues
-        aligned_blocks = []
+        page_types = []
         for i in range(len(boxes)):
             unnorm_box = unnormalize_box(boxes[i], pwidth, pheight)
             appended = False
             for j in range(len(predicted_block_types)):
                 if boxes[i] == predicted_block_types[j].bbox:
                     predicted_block_types[j].bbox = unnorm_box
-                    aligned_blocks.append(predicted_block_types[j])
+                    page_types.append(predicted_block_types[j])
                     appended = True
                     break
             if not appended:
-                aligned_blocks.append(BlockType(
-                    block_type="Text",
-                    bbox=unnorm_box
-                ))
-        page_block_types.append(aligned_blocks)
+                page_types.append(BlockType(block_type="Text", bbox=unnorm_box))
+        pages_types.append(page_types)
         page_start += sample_length
-    return page_block_types
-
+    return pages_types
