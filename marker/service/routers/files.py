0ba8677b369c1053f75c3c425f40ac513a09db53
遥奕 <jibing.yjb@alibaba-inc.com>
2024-01-13 15:00:47 +0800

    marker support download pdf from a presigned url and put markdown back to a presigned url

--- marker/service/routers/files.py
+++ marker/service/routers/files.py
@@ -1,7 +1,11 @@
+import os
+import uuid
 from fastapi import APIRouter
 from pydantic import BaseModel
 
 import time
+
+import requests
 
 from marker.convert import convert_single_pdf
 from marker.models import load_all_models
@@ -27,6 +31,7 @@ class PDFFile(BaseModel):
     outFileName: str
     maxPages: int
     parallelFactor: int
+    isDebug: bool = False
 
 
 @router.post("/parser/", tags=["pdf parser"])
@@ -102,3 +107,83 @@ def upload_file(source_file: str, destination_file: str):
         "to bucket",
         bucket_name,
     )
+
+
+@router.post("/v2/parser/", tags=["pdf parser"])
+async def post_v2_parser(pdf_file: PDFFile):
+    uuid_str = str(uuid.uuid4())
+    local_pdf_file: str = uuid_str + ".pdf"
+    local_md_file: str = uuid_str + ".md"
+    download_presigned_file(
+        pdf_file.inFileName,
+        local_pdf_file,
+    )
+
+    start_time = time.time()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(
+        f"Function '{load_all_models.__name__}' took {execution_time} seconds to execute."
+    )
+
+    start_time = time.time()
+    full_text, out_meta = convert_single_pdf(
+        local_pdf_file,
+        model_lst,
+        max_pages=pdf_file.maxPages,
+        parallel_factor=pdf_file.parallelFactor,
+    )
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(
+        f"Function '{convert_single_pdf.__name__}' took {execution_time} seconds to execute."
+    )
+
+    with open(local_md_file, "w+", encoding="utf-8") as f:
+        f.write(full_text)
+
+    upload_presigned_file(pdf_file.outFileName, local_md_file)
+    if not pdf_file.isDebug:
+        delete_file(local_pdf_file)
+        delete_file(local_md_file)
+    return {"code": "200", "message": "success"}
+
+
+def download_presigned_file(signed_url, local_file_path):
+    try:
+        response = requests.get(signed_url)
+
+        if response.status_code == 200:
+            # 将响应内容写入本地文件
+            with open(local_file_path, "wb") as file:
+                file.write(response.content)
+            print(f"Successfully downloaded file to {local_file_path}")
+        else:
+            print(f"Failed to download file. Status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"Error downloading file: {e}")
+
+
+def upload_presigned_file(presigned_url, local_file_path):
+    try:
+        with open(local_file_path, "rb") as file:
+            response = requests.put(presigned_url, data=file)
+
+        if response.status_code == 200:
+            print(f"Successfully uploaded file using presigned URL.")
+        else:
+            print(
+                f"Failed to upload file using presigned URL. Status code: {response.status_code}"
+            )
+
+    except Exception as e:
+        print(f"Error uploading file using presigned URL: {e}")
+
+
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+        print(f"Successfully deleted file: {file_path}")
+    except Exception as e:
+        print(f"Error deleting file: {e}")
