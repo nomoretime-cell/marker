527012aed89ecb58a7b43ca2f4944fbcf5d99735
遥奕 <jibing.yjb@alibaba-inc.com>
2024-01-15 19:50:51 +0800

    optimize the size of docker image(14G -> 7G)

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --gpus device=1 -d -it -v ./model:/marker/model -p 8002:8000 --name pdf-parser marker:1.0
+docker run --gpus device=1 -d -it -e OCR_ALL_PAGES=False -v ./model:/marker/model -p 8002:8000 --name pdf-parser marker:1.0
--- scripts/install/ghostscript_install.sh
+++ scripts/install/ghostscript_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs10012/ghostscript-10.01.2.tar.gz
+file="ghostscript-10.01.2.tar.gz"
+
+if [ ! -f "$file" ]; then
+    wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs10012/ghostscript-10.01.2.tar.gz
+fi
 tar -xvf ghostscript-10.01.2.tar.gz
 cd ghostscript-10.01.2
 ./configure
