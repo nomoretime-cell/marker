590d2d41f81ef3a61cbd0b174ed4fc51e7936d34
遥奕 <jibing.yjb@alibaba-inc.com>
2024-01-18 17:35:43 +0800

    add model to docker image

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
   docker build \
-  -t marker:1.0 -f Dockerfile ..
+  -t marker:1.0-model -f Dockerfile ..
--- run-docker.sh
+++ run-docker.sh
@@ -5,7 +5,6 @@ docker run \
   --gpus device=1 \
   -e OCR_ALL_PAGES=False \
   -e WORKER_NUM=4 \
-  -v ./model:/marker/model \
   -p 8002:8000 \
   --name pdf-parser \
-  marker:1.0
+  marker:1.0-model
