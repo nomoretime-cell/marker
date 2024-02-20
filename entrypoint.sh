025ce4e0535922020db6732404d95f24cca86ec9
遥奕 <jibing.yjb@alibaba-inc.com>
2024-02-20 12:09:00 +0800

    [bugfix] [taskId]: none [taskUrl]: none [describe]: increase gunicorn timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 POETRY_CMD="poetry run gunicorn \
              service:app \
-             -t 600 \
+             --timeout 7200 \
              --workers ${WORKER_NUM} \
              --worker-class uvicorn.workers.UvicornWorker \
              --bind 0.0.0.0:8000"
