f525648484a69e3cb714c91e03ab39d8b580681e
遥奕 <jibing.yjb@alibaba-inc.com>
2024-02-27 21:00:22 +0800

    [feature] [taskId]: none [taskUrl]: none [describe]: support filter specific file from file list

--- client/main.py
+++ client/main.py
@@ -13,27 +13,50 @@ if __name__ == "__main__":
     # Queue
     message_queue: queue.Queue = queue.Queue(maxsize=1000)
 
-    # Producer
     minio_client: S3Client = S3Client(
         config_reader.get_value("OSS", "endpoint"),
         config_reader.get_value("OSS", "access_key"),
         config_reader.get_value("OSS", "secret_key"),
     )
-    producer_thread = threading.Thread(
-        target=minio_client.prepare_object,
-        args=(
-            message_queue,
-            config_reader.get_value("OSS", "bucket"),
-            config_reader.get_value("OSS", "folder_path"),
-            config_reader.get_value("OSS", "out_folder_path"),
-            config_reader.get_value("OSS", "file_type").split(","),
-            None
-            if int(config_reader.get_value("Common", "limit")) <= 0
-            else int(config_reader.get_value("Common", "limit")),
-            True,
-            3600 * 24 * 7 - 3600,
-        ),
-    )
+    # Producer
+    if config_reader.get_value("Common", "filter_mode") == "True":
+        producer_thread = threading.Thread(
+            target=minio_client.filter_object,
+            args=(
+                message_queue,
+                config_reader.get_value("OSS", "bucket"),
+                config_reader.get_value("OSS", "folder_path"),
+                config_reader.get_value("OSS", "out_folder_path"),
+                config_reader.get_value("OSS", "file_type").split(","),
+                None
+                if int(config_reader.get_value("Common", "limit")) <= 0
+                else int(config_reader.get_value("Common", "limit")),
+                {
+                    "fileType": config_reader.get_value("DocFilter", "fileType"),
+                    "contentType": config_reader.get_value("DocFilter", "contentType"),
+                    "language": config_reader.get_value("DocFilter", "language"),
+                    "columnNum": int(config_reader.get_value("DocFilter", "columnNum")),
+                },
+                True,
+                3600 * 24 * 7 - 3600,
+            ),
+        )
+    else:
+        producer_thread = threading.Thread(
+            target=minio_client.prepare_object,
+            args=(
+                message_queue,
+                config_reader.get_value("OSS", "bucket"),
+                config_reader.get_value("OSS", "folder_path"),
+                config_reader.get_value("OSS", "out_folder_path"),
+                config_reader.get_value("OSS", "file_type").split(","),
+                None
+                if int(config_reader.get_value("Common", "limit")) <= 0
+                else int(config_reader.get_value("Common", "limit")),
+                True,
+                3600 * 24 * 7 - 3600,
+            ),
+        )
 
     # Consumer
     if config_reader.get_value("Common", "presigned_mode") == "True":
--- client/s3_client.py
+++ client/s3_client.py
@@ -1,6 +1,7 @@
+import json
 import queue
 from datetime import timedelta
-from typing import Generator, List
+from typing import Generator
 from minio import Minio
 from minio.error import MinioException
 from client.structure import MessageBody
@@ -12,35 +13,13 @@ class S3Client:
     ) -> None:
         if not endpoint or not access_key or not secret_key:
             raise ValueError("'endpoint' or 'access key' or 'secret key' is Empty")
-
         self.minio_client: Minio = Minio(
             endpoint,
             access_key,
             secret_key,
             secure=secure,
         )
-
-    def list_objects_path(
-        self, bucket_name: str, folder_path: str, file_type: list[str], limit: int = None
-    ) -> Generator[str, None, None]:
-        objects: list = self.minio_client.list_objects(
-            bucket_name, prefix=folder_path, recursive=True
-        )
-        obj_count = 0
-        for obj in objects:
-            obj_name: str = obj.object_name
-            
-            contained = False
-            for type in file_type:
-                if obj_name.endswith(type):
-                    contained = True
-                    break
-            if not contained:
-                continue
-            obj_count += 1
-            yield obj_name
-            if limit is not None and obj_count >= limit:
-                break
+        self.file_cache: list[str] = []
 
     def replace_extension(self, filename, new_extension=".md"):
         dot_index = filename.rfind(".")
@@ -48,38 +27,6 @@ class S3Client:
             return filename[:dot_index] + new_extension
         else:
             return filename + new_extension
-
-    def prepare_object(
-        self,
-        queue: queue.Queue[MessageBody],
-        bucket_name: str,
-        folder_path: str,
-        out_folder_path: str,
-        file_type: list[str],
-        limit: int = None,
-        signed: bool = False,
-        expiration: int = 3600,
-    ) -> None:
-        for object_path in self.list_objects_path(
-            bucket_name, folder_path, file_type, limit
-        ):
-            file_original_name = object_path.rsplit("/", 1)[-1]
-            out_object_path = out_folder_path + self.replace_extension(
-                file_original_name
-            )
-
-            if not signed:
-                queue.put(MessageBody(file_original_name, object_path, out_object_path))
-            else:
-                queue.put(
-                    MessageBody(
-                        file_original_name,
-                        self.get_presigned_url(bucket_name, object_path, expiration),
-                        self.generate_presigned_url(
-                            bucket_name, out_object_path, expiration
-                        ),
-                    )
-                )
 
     def get_presigned_url(
         self, bucket_name: str, object_name: str, expiration: int
@@ -104,3 +51,114 @@ class S3Client:
         except MinioException as e:
             print(f"Error generating presigned URL: {e}")
             return None
+
+    def list_objects_path(
+        self,
+        bucket_name: str,
+        folder_path: str,
+        file_type: list[str],
+        limit: int = None,
+    ) -> Generator[str, None, None]:
+        objects: list = self.minio_client.list_objects(
+            bucket_name, prefix=folder_path, recursive=True
+        )
+        obj_count = 0
+        for obj in objects:
+            obj_name: str = obj.object_name
+
+            contained = False
+            for type in file_type:
+                if obj_name.endswith(type):
+                    contained = True
+                    break
+            if not contained:
+                continue
+            obj_count += 1
+            yield obj_name
+            if limit is not None and obj_count >= limit:
+                break
+
+    def prepare_msg(
+        self,
+        queue: queue.Queue[MessageBody],
+        bucket_name: str,
+        object_path: str,
+        out_folder_path: str,
+        signed: bool = False,
+        expiration: int = 3600,
+    ):
+        file_original_name = object_path.rsplit("/", 1)[-1]
+        out_object_path = out_folder_path + self.replace_extension(file_original_name)
+
+        if not signed:
+            queue.put(MessageBody(file_original_name, object_path, out_object_path))
+        else:
+            queue.put(
+                MessageBody(
+                    file_original_name,
+                    self.get_presigned_url(bucket_name, object_path, expiration),
+                    self.generate_presigned_url(
+                        bucket_name, out_object_path, expiration
+                    ),
+                )
+            )
+
+    def prepare_object(
+        self,
+        queue: queue.Queue[MessageBody],
+        bucket_name: str,
+        folder_path: str,
+        out_folder_path: str,
+        file_type: list[str],
+        limit: int = None,
+        signed: bool = False,
+        expiration: int = 3600,
+    ) -> None:
+        for object_path in self.list_objects_path(
+            bucket_name, folder_path, file_type, limit
+        ):
+            self.prepare_msg(
+                queue, bucket_name, object_path, out_folder_path, signed, expiration
+            )
+
+    def filter_object(
+        self,
+        queue: queue.Queue[MessageBody],
+        bucket_name: str,
+        folder_path: str,
+        out_folder_path: str,
+        file_type: list[str],
+        limit: int = None,
+        filter_key: dict = None,
+        signed: bool = False,
+        expiration: int = 3600,
+    ) -> None:
+        for object_path in self.list_objects_path(
+            bucket_name, folder_path, file_type, limit
+        ):
+            object_list: list = []
+            # get file list from folder
+            file = self.minio_client.get_object(bucket_name, object_path)
+            try:
+                for line in file.stream(512 * 1024 * 1024):
+                    for json_object in line.decode("utf-8").splitlines():
+                        json_object = json.loads(json_object)
+                        filter_flag = False
+                        for key, value in filter_key.items():
+                            if json_object[key] != value:
+                                filter_flag = True
+                                break
+                        if filter_flag:
+                            continue
+                        if json_object["path"].rsplit("/", 1)[-1] not in self.file_cache:
+                            self.file_cache.append(json_object["path"].rsplit("/", 1)[-1])
+                            object_list.append(json_object)
+            except Exception as e:
+                print(f"Error loading json: {e}")
+            finally:
+                file.close()
+                file.release_conn()
+            for obj in object_list:
+                self.prepare_msg(
+                    queue, obj["bucket"], obj["path"], out_folder_path, signed, expiration
+                )
