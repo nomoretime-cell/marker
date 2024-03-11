e0a07fc86eb6980fc53a0f5a8114adedda2cfae5
遥奕 <jibing.yjb@alibaba-inc.com>
2024-03-11 15:02:40 +0800

    [feature] [taskId]: none [taskUrl]: none [describe]: support uploading form-data pdf file

--- marker/service/routers/parser.py
+++ marker/service/routers/parser.py
@@ -3,8 +3,10 @@ import logging
 import os
 import threading
 import time
+import uuid
+import aiofiles
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, File, HTTPException, UploadFile
 from marker.convert import convert_single_pdf
 from marker.models import load_all_models
 from marker.service.func.common_func import (
@@ -23,7 +25,7 @@ is_request_processing = False
 is_request_processing_lock = threading.Lock()
 
 
-@parser_router.post("/internal/parser/", tags=["doc parser"])
+@parser_router.post("/internal/parser", tags=["doc parser"])
 async def post_parser(parser_request: ParserRequest) -> dict:
     logging.info(f"POST request, thread id: {threading.current_thread().ident}")
     loop = asyncio.get_running_loop()
@@ -33,8 +35,8 @@ async def post_parser(parser_request: ParserRequest) -> dict:
         None, download_file, parser_request.inFileUrl, parser_request.inFileUrl
     )
 
-    # 2. process
-    full_text, out_meta = process(parser_request.inFileUrl, parser_request)
+    # 2. inner_process
+    full_text, out_meta = inner_process(parser_request.inFileUrl, parser_request)
 
     # 3. save file
     await loop.run_in_executor(None, save_file, parser_request.outFileUrl, full_text)
@@ -50,8 +52,37 @@ async def post_parser(parser_request: ParserRequest) -> dict:
     return ParserResponse(parser_request.requestId, "200", "success").to_dict()
 
 
-@parser_router.post("/v1/parser/", tags=["doc parser"])
+@parser_router.post("/v1/parser", tags=["doc parser"])
 async def post_v1_parser(parser_request: ParserRequest) -> dict:
+    return await process(parser_request)
+
+
+@parser_router.post("/v1/parser/file", tags=["doc parser"])
+async def post_v1_parser_file(file: UploadFile = File(...)) -> dict:
+    parser_request: ParserRequest = ParserRequest(
+        requestId=str(uuid.uuid4()),
+        formDataFile=file,
+    )
+    return await process(parser_request)
+
+
+def inner_process(file_path: str, parser_request: ParserRequest) -> tuple[str, dict]:
+    start_time: float = time.time()
+    full_text, out_meta = convert_single_pdf(
+        file_path,
+        model_lst,
+        max_pages=parser_request.maxPages,
+        parallel_factor=parser_request.parallelFactor,
+        debug_mode=parser_request.isDebug,
+    )
+    execution_time: float = time.time() - start_time
+    logging.info(
+        f"Function '{convert_single_pdf.__name__}' took {execution_time} seconds to execute."
+    )
+    return full_text, out_meta
+
+
+async def process(parser_request: ParserRequest):
     global is_request_processing
     with is_request_processing_lock:
         if is_request_processing:
@@ -72,47 +103,41 @@ async def post_v1_parser(parser_request: ParserRequest) -> dict:
         local_original_file: str = parser_request.requestId
 
         # 2. download file
-        download_presigned_file(parser_request.inFileUrl, local_original_file)
+        if parser_request.formDataFile:
+            async with aiofiles.open(local_original_file, "wb") as out_file:
+                content = await parser_request.formDataFile.read()
+                await out_file.write(content)
+        else:
+            download_presigned_file(parser_request.inFileUrl, local_original_file)
 
-        # 3. process
+        # 3. inner_process
         full_text, out_meta = await loop.run_in_executor(
-            None, process, local_original_file, parser_request
+            None, inner_process, local_original_file, parser_request
         )
 
         # 4. upload markdown file
-        if not parser_request.isDebug:
+        if parser_request.isDebug:
+            # save markdown file
+            local_result_file: str = parser_request.requestId + ".md"
+            save_file(local_result_file, full_text)
+        else:
+            # delete pdf file
             delete_file(local_original_file)
+
+        if not parser_request.formDataFile:
             upload_presigned_file(
                 parser_request.outFileUrl,
                 "",
                 full_text,
             )
-        else:
-            local_result_file: str = parser_request.requestId + ".md"
-            save_file(local_result_file, full_text)
-            upload_presigned_file(parser_request.outFileUrl, local_result_file)
 
     except HTTPException as http_e:
         raise http_e
     except Exception as e:
-        ParserResponse(parser_request.requestId, "500", e).to_dict()
+        return ParserResponse(parser_request.requestId, "500", e).to_dict()
     finally:
         with is_request_processing_lock:
             is_request_processing = False
-    return ParserResponse(parser_request.requestId, "200", "success").to_dict()
-
-
-def process(file_path: str, parser_request: ParserRequest) -> tuple[str, dict]:
-    start_time: float = time.time()
-    full_text, out_meta = convert_single_pdf(
-        file_path,
-        model_lst,
-        max_pages=parser_request.maxPages,
-        parallel_factor=parser_request.parallelFactor,
-        debug_mode=parser_request.isDebug,
-    )
-    execution_time: float = time.time() - start_time
-    logging.info(
-        f"Function '{convert_single_pdf.__name__}' took {execution_time} seconds to execute."
-    )
-    return full_text, out_meta
+        return ParserResponse(
+            parser_request.requestId, "200", "success", data={"text": full_text}
+        ).to_dict()
--- marker/service/struct/parser_struct.py
+++ marker/service/struct/parser_struct.py
@@ -1,27 +1,33 @@
 from typing import Optional
+from fastapi import UploadFile
 from pydantic import BaseModel
 
 
 class ParserRequest(BaseModel):
     requestId: str
-    inFileUrl: str
-    outFileUrl: str
+    inFileUrl: str = None
+    outFileUrl: str = None
+    formDataFile: UploadFile = None
     maxPages: Optional[int] = None
     parallelFactor: Optional[int] = 1
     isDebug: Optional[bool] = False
 
 
 class ParserResponse:
-    def __init__(self, requestId: str, code: str, message: str) -> None:
+    def __init__(
+        self, requestId: str, code: str, message: str, data: dict = None
+    ) -> None:
         self.requestId: str = requestId
         self.code: str = code
         self.message: str = message
+        self.data: dict = data
 
     def to_dict(self) -> dict:
         obj_dict = {
             "requestId": self.requestId,
             "code": self.code,
             "message": self.message,
+            "data": self.data,
         }
 
         return obj_dict
